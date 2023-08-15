e82e6fecd66ddf58930b4a90839e98786daa2ed6
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-08-15 16:21:48 -0400

    lastIndexOf.js exercise

---
+++ lastIndexOf.js
@@ -0,0 +1,15 @@
+const lastIndexOf = (arr, val) => {
+  let exists = -1;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] === val){
+      exists = i;
+    }
+  }
+  return exists;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
